5cb634b8d62fdae45441165861d2421b6aef0f29
inah <67313423+nanahb@users.noreply.github.com>
2023-04-08 06:10:01 +0000

    Made activeLink variant style work for navButton.

--- src/components/NavButton.js
+++ src/components/NavButton.js
@@ -2,10 +2,18 @@
 import React from "react";
 import { Link } from 'gatsby'
 import * as styles from "./NavButton.module.css";
+/**
+ * 
+ * Option A: refactor our css to use nested selectors for the active style.
+ * .navbutton--state-active {styles for this}
+ * change to 
+ * .navbutton {default navbutton style}
+ * .activeLink .navbutton {style for active}
+ */
 export const NavButton = ({ state = "default", text = "Home", to='/', ...props }) => {
   const variantsClassName = "navbutton--state-" + state;
   return (
-    <Link to={to}>
+    <Link to={to} activeClassName={styles["activeLink"]}>
     <div className={styles["navbutton"] + " " + styles[variantsClassName]}>
       
         <div className={styles["rectangle"]}></div>
